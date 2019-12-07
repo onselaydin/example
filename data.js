35eed87580376314e79512a86a0a4f0095aa3e2d
Önsel AYDIN <onselaydin@gmail.com>
2019-12-07 22:39:48 +0300

    json-server

---
+++ data.js
@@ -0,0 +1,14 @@
+module.exports = function() {
+    return {
+        products:[
+            { id:1, name:"Samsun S5", price:1000, imageUrl:"1.jpg", description:"idare eder."},
+            { id:1, name:"Samsun S6", price:3000, imageUrl:"1.jpg", description:"idare eder."},
+            { id:1, name:"Samsun S7", price:1000, imageUrl:"1.jpg", description:"idare eder."},
+        ],
+        categories: [
+            {id:1, name:"Telefon"},
+            {id:2, name:"Bilgisayar"},
+            {id:3, name:"Elektronik"}
+        ]
+    }
+}
