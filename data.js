f8730c2db6d6475d9cf05269cd3e53652727e062
Önsel AYDIN <onselaydin@gmail.com>
2020-02-21 17:58:00 +0300

    product component

--- data.js
+++ data.js
@@ -1,9 +1,9 @@
 module.exports = function() {
     return {
         products:[
-            { id:1, name:"Samsun S5", price:1000, imageUrl:"1.jpg", description:"idare eder."},
-            { id:1, name:"Samsun S6", price:3000, imageUrl:"1.jpg", description:"idare eder."},
-            { id:1, name:"Samsun S7", price:1000, imageUrl:"1.jpg", description:"idare eder."},
+            { id:1, name:"Samsun S5", price:1000, imageUrl:"1.jpg", categoryid:1, description:"idare eder."},
+            { id:1, name:"Lenovo", price:3000, imageUrl:"1.jpg", categoryid:2, description:"lenovo dokanmatik"},
+            { id:1, name:"Samsun S7", price:1000, imageUrl:"1.jpg", categoryid:1, description:"idare eder."},
         ],
         categories: [
             {id:1, name:"Telefon"},
--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product.component.css']
 })
 export class ProductComponent implements OnInit {
+  title="Ürünler";
+  noproduct="Yok";
+  name="test";
+  products:any[]=[
+    { id:1, name:"Samsun S5", price:1000, imageUrl:"1.jpg", categoryid:1, description:"idare eder."},
+    { id:1, name:"Lenovo", price:3000, imageUrl:"1.jpg", categoryid:2, description:"lenovo dokanmatik"},
+    { id:1, name:"Samsun S7", price:1000, imageUrl:"1.jpg", categoryid:1, description:"idare eder."},
+];
 
-  constructor() { }
-  name = "test deneme";
+  constructor() { 
+
+  }
   ngOnInit(): void {
+
   }
 
 }
